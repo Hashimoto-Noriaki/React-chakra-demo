71e454d958983e1f4a440ef580e1d61397646ca0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-15 23:30:26 +0900

    ルーティングの設定

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,24 @@
 import { memo,VFC } from "react"
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { Home } from "../components/pages/Home"; 
+import { homeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
     return(
         <Routes>
+            {/* ルート "/" は Login コンポーネントを表示 */}
              <Route path="/" element={<Login />} />
+            {/* ルート "/home" は Home コンポーネントとネストされたルートを表示 */}       
+             <Route path="/home" element={<Home />}/>
+            {/* ネストされたルート */}
+            {homeRoutes.map((route) => (
+                <Route
+                key={route.path}
+                path={route.path}
+                element={route.children}
+                />
+            ))}
         </Routes>
     )
 })
