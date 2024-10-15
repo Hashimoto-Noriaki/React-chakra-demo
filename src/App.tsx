a087d0188df4cd3abab73f942e5ec926ba16a5aa
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-10-15 19:28:38 +0900

    App.tsxにBrowserProviderを追記

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ChakraProvider, Button } from "@chakra-ui/react";
 import theme from "./theme/theme";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>テスト</p>
-      <b>Reactテスト</b>
+      <BrowserRouter>
+        <Button colorScheme="teal">ボタン</Button>
+        <p>テスト</p>
+        <b>Reactテスト</b>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
